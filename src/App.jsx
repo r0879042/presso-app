a5ce1a4a6aeea832a1158d2ae882e4426bbaa931
Umutesi Sonia <r0879042@student.thomasmore.be>
2025-05-26 00:01:12 +0200

    current frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Find from './pages/Find';
 import Vertuo from './pages/Vertuo';
